b21a0eb10964234bec5bd85345d70aab048e59fc
Kacper Drejer <kacper.drejer@gmail.com>
2020-11-01 20:54:41 +0100

    Comment timeout in bash script

--- train.sh
+++ train.sh
@@ -4,15 +4,16 @@ sets=(celeba)
 for set in ${sets[*]};
 do
     echo "Start training on $set."
-    for i in $(seq 1 3);
-    do
-        echo "Running $i turn of training."
-        timeout 2h python train.py --cfg experiments/cfgs/gans/$set.yml --is_train
-        echo "Finished $i turn of training."
-        sleep 20m;
+    #for i in $(seq 1 3);
+    #do
+        # echo "Running $i turn of training."
+        # timeout 2h 
+    python train.py --cfg experiments/cfgs/gans/$set.yml --is_train
+        #echo "Finished $i turn of training."
+        #sleep 20m;
 
-    done
-    echo "Training on $set finished."
+    #done
+    #echo "Training on $set finished."
 done
 
 shutdown -h now
