Fix scaling RGB to normal

blackbox.py
@@ -239,9 +239,9 @@ def get_celeba(data_path, test_on_dev=True, orig_data=False):
     dev_name = 'val'
     if not test_on_dev:
         dev_name = 'test'
-    ds = CelebA(attribute=FLAGS.attribute)
+    ds = CelebA(attribute=FLAGS.attribute, scale_rgb_to_normal=True)
     ds.load()
-    ds_test = CelebA(attribute=FLAGS.attribute)
+    ds_test = CelebA(attribute=FLAGS.attribute, scale_rgb_to_normal=True)
     ds_test.load(split=dev_name)
     train_labels = ds.labels
     test_labels = ds_test.labels
@@ -255,7 +255,7 @@ def get_celeba(data_path, test_on_dev=True, orig_data=False):
             np.int32)
         pkl_paths = [os.path.join(train_data_path, pf) for pf in
                      sorted(pkl_files)]
-        pkl_ds = PickleLazyDataset(pkl_paths, [64, 64, 3])
+        pkl_ds = PickleLazyDataset(pkl_paths, [64, 64, 3], True)
         return pkl_ds, pkl_labels
 
     if orig_data:

datasets/celeba.py
@@ -25,7 +25,7 @@ from datasets.dataset import Dataset, LazyDataset
 class CelebA(Dataset):
     """CelebA class implementing Dataset."""
 
-    def __init__(self, center_crop_size=108, resize_size=64, attribute=None):
+    def __init__(self, center_crop_size=108, resize_size=64, attribute=None, scale_rgb_to_normal=False):
         """CelebA constructor.
 
         Args:
@@ -41,6 +41,7 @@ class CelebA(Dataset):
         self.split_data = {}
         self.image_size = center_crop_size
         self.resize_size = resize_size
+        self.scale_rgb_to_normal = scale_rgb_to_normal
         # The attribute represents which attribute to use in case of
         # classification.
         self.attribute = attribute
@@ -94,7 +95,7 @@ class CelebA(Dataset):
             np.random.shuffle(fps)
             np.random.set_state(rng_state)
 
-        images = LazyDataset(fps, self.image_size, self.resize_size)
+        images = LazyDataset(fps, self.image_size, self.resize_size, scale_rgb_to_normal=self.scale_rgb_to_normal)
         # Access images if not lazy.
         if not lazy:
             images = images[:len(images)]